dc21d896e3ec2167bbd00e294af96332582aec9c
Abhijeet Choudhary <achoudhary@pippintitle.com>
2026-06-01 15:41:58 +0530

    initial commit

---
+++ hello.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log("Hello world i am starting to learn about tsc --> type script compiler!!");
+// This is an industrial-grade general-purpose greeter function:
+function greet(person, date) {
+    console.log(`Hello ${person}, today is ${date}!`);
+}
+greet("Brendan", new Date());
---
+++ hello.ts
@@ -0,0 +1,8 @@
+console.log("Hello world i am starting to learn about tsc --> type script compiler!!");
+
+// This is an industrial-grade general-purpose greeter function:
+function greet(person : string, date : Date) {
+  console.log(`Hello ${person}, today is ${date}!`);
+}
+ 
+greet("Brendan", new Date());
